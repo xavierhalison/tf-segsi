c0f1c8d1bd32022ad5a6ac2f2d9e7b1fe8461633
xavierhalison <halisonpx@gmail.com>
2020-10-16 10:52:06 -0300

    testing firebase auth

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,28 @@
-const functions = require("firebase-functions");
+var admin = require("firebase-admin");
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+var serviceAccount = require("./xavier-ddf46-firebase-adminsdk-lnpmi-7143b648d3.json");
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://xavier-ddf46.firebaseio.com",
 });
 
+// Backend
+var firebaseAdmin = require("firebase-admin");
+var express = require("express");
+var router = express.Router();
+
+router.post("/token", function (request, response, next) {
+  if (true) {
+    firebaseAdmin
+      .auth()
+      .createCustomToken(request.body.login)
+      .then(function (token) {
+        response.json({ token: token });
+      })
+      .catch(function (error) {
+        res.status(500).json({ error: "Error during token creation" });
+      });
+  }
+});
+module.exports = router;
